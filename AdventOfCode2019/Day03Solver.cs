b8bc61582e706afad8b96928f791f81494447e40
ALEJANDRO CARRIÓN SANMARTÍN <alcarsa5@upv.edu.es>
2020-06-27 12:43:46 +0200

    Day3

---
+++ AdventOfCode2019/Day03Solver.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2019
+{
+    public class Day3Solver : Solver
+    {
+        string[] path1;
+        string[] path2;
+
+        public Day3Solver(string input)
+        {
+            path1 = input.Split('\n')[0].Split(',');
+            path2 = input.Split('\n')[1].Split(',');
+        }
+
+
+        public override int SolvePart1()
+        {
+            List<Point> path1Points = GetVisitedPoints(path1);
+            List<Point> path2Points = GetVisitedPoints(path2);
+
+            List<Point> commonPoints = (from p in path1Points where path2Points.Contains(p) select p).ToList();
+
+            return commonPoints.Min(p => Math.Abs(p.x) + Math.Abs(p.y));
+        }
+
+        public override int SolvePart2()
+        {
+            List<Point> path1Points = GetVisitedPoints(path1);
+            List<Point> path2Points = GetVisitedPoints(path2);
+
+            List<Point> commonPoints = (from p in path1Points where path2Points.Contains(p) select p).ToList();
+
+            return commonPoints.Min(p => StepsToReach(p));
+        }
+
+
+        int StepsToReach(Point p)
+        {
+            return StepsToReachInPath(p, path1) + StepsToReachInPath(p, path2);
+        }
+
+        int StepsToReachInPath(Point p, string[] path)
+        {
+            int totalSteps = 0;
+
+            Point currentPosition = new Point(0, 0);
+            for (int movement = 0; movement < path.Length && !p.Equals(currentPosition); movement++)
+            {
+                char direction = GetDirection(path[movement]);
+                int steps = GetNumberOfSteps(path[movement]);
+
+                int newX = currentPosition.x, newY = currentPosition.y;
+
+                for (int i = 0; i < steps; i++)
+                {
+                    switch (direction)
+                    {
+                        case 'U': newY++; break;
+                        case 'D': newY--; break;
+                        case 'R': newX++; break;
+                        case 'L': newX--; break;
+                    }
+
+                    totalSteps++;
+                    if (p.Equals(new Point(newX, newY))) break;
+                }
+
+                currentPosition = new Point(newX, newY);
+            }
+
+            return totalSteps;
+        }
+
+        List<Point> GetVisitedPoints(string[] path)
+        {
+            List<Point> visitedPoints = new List<Point>();
+            Point currentPosition = new Point(0, 0);
+
+            for (int movement = 0; movement < path.Length; movement++)
+            {
+                char direction = GetDirection(path[movement]);
+                int steps = GetNumberOfSteps(path[movement]);
+
+                int newX = currentPosition.x, newY = currentPosition.y;
+
+                for (int i = 0; i < steps; i++)
+                {   
+                    switch (direction)
+                    {
+                        case 'U': newY++; break;
+                        case 'D': newY--; break;
+                        case 'R': newX++; break;
+                        case 'L': newX--; break;
+                    }
+
+                    visitedPoints.Add(new Point(newX, newY));
+                }
+
+                currentPosition = new Point(newX, newY);
+            }
+            
+            return visitedPoints;
+        }
+
+        int GetNumberOfSteps(string movement) { return int.Parse(movement.Substring(1)); }
+        char GetDirection(string movement) { return movement[0]; }
+    }
+
+    class Point
+    {
+        public int x, y;
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Point pair &&
+                   x == pair.x &&
+                   y == pair.y;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(x, y);
+        }
+    }
+}
---
+++ AdventOfCode2019Tests/Day03SolverTests.cs
@@ -0,0 +1,41 @@
+﻿using AdventOfCode2019;
+using AdventOfCode2019Tests.Properties;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventOfCode2019Tests
+{
+    [TestClass]
+    public class Day03SolverTests
+    {
+        [TestMethod]
+        [DataRow("R8,U5,L5,D3\nU7,R6,D4,L4", 6)]
+        [DataRow("R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83", 159)]
+        [DataRow("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7", 135)]
+        public void ManhattanDistanceTest(string paths, int expectedManhattanDistance)
+        {
+            Assert.AreEqual((new Day3Solver(paths)).SolvePart1(), expectedManhattanDistance);
+        }
+
+        [TestMethod]
+        [DataRow("R8,U5,L5,D3\nU7,R6,D4,L4", 30)]
+        [DataRow("R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83", 610)]
+        [DataRow("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7", 410)]
+        public void FewestStepsTest(string paths, int expectedSteps)
+        {
+            Assert.AreEqual((new Day3Solver(paths)).SolvePart2(), expectedSteps);
+        }
+
+
+        [TestMethod]
+        public void Part1Test()
+        {
+            Assert.AreEqual((new Day3Solver(Resources.Day3Input)).SolvePart1(), 627);
+        }
+
+        [TestMethod]
+        public void Part2Test()
+        {
+            Assert.AreEqual((new Day3Solver(Resources.Day3Input)).SolvePart2(), 13190);
+        }
+    }
+}
