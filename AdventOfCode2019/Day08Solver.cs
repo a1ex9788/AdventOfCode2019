000484cd2ebcfca8cda0abea8b45aa2676261be3
ALEJANDRO CARRIÓN SANMARTÍN <alcarsa5@upv.edu.es>
2020-07-06 18:46:33 +0200

    Day 8

---
+++ AdventOfCode2019/Day08Solver.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+
+namespace AdventOfCode2019
+{
+    public class Day8Solver : Solver
+    {
+        static int BLACK_PIXEL = 0, WHITE_PIXEL = 1, TRANSPARENT_PIXEL = 2;
+
+        string input;
+        int pixelsWide, pixelsTall;
+        List<Layer> layers;
+
+        public Day8Solver(string input, int pixelsWide, int pixelsTall)
+        {
+            this.input = input;
+            this.pixelsWide = pixelsWide;
+            this.pixelsTall = pixelsTall;
+            
+            ConstructLayers();
+        }
+
+
+        public override int SolvePart1()
+        {
+            Layer fewNumberOf0Layer = layers[0];
+            int fewNumberOf0 = int.MaxValue;
+
+            foreach (Layer l in layers)
+            {
+                int numberOf0 = l.CountAparitions(0);
+
+                if (numberOf0 < fewNumberOf0)
+                {
+                    fewNumberOf0Layer = l;
+                    fewNumberOf0 = numberOf0;
+                }
+            }
+
+            return fewNumberOf0Layer.CountAparitions(1) * fewNumberOf0Layer.CountAparitions(2);
+        }
+
+        public override int SolvePart2()
+        {
+            string finalLayer = "";
+
+            for (int i = 0; i < pixelsTall; i++)
+                for (int j = 0; j < pixelsWide; j++)
+                    finalLayer += CalculeFinalPixel(j, i);
+
+            string res = (new Layer(finalLayer, pixelsWide, pixelsTall)).ToString();
+
+            Console.WriteLine(res);
+            return res.GetHashCode();
+        }
+
+
+        public string ConstructLayers()
+        {
+            layers = new List<Layer>();
+
+            for (int i = 0; i < input.Length; i += pixelsWide * pixelsTall)
+                layers.Add(new Layer(input.Substring(i, pixelsWide * pixelsTall), pixelsWide, pixelsTall));
+
+            string res = "";
+            foreach (Layer l in layers)
+                res += l.ToString() + "\n\n";
+
+            return res.Substring(0, res.Length - 2);
+        }
+
+        private string CalculeFinalPixel(int i, int j)
+        {
+            int pixelNumber = layers[0].GetNumber(i, j); ;
+            int layerNumber = 0;
+
+            while (pixelNumber == TRANSPARENT_PIXEL)
+            {
+                layerNumber++;
+
+                pixelNumber = layers[layerNumber].GetNumber(i, j);
+            }
+
+            return pixelNumber.ToString();
+        }
+    }
+}
+
+class Layer
+{
+    int[,] rows;
+    int pixelsWide, pixelsTall;
+
+    public Layer(string numbers, int pixelsWide, int pixelsTall)
+    {
+        rows = new int[pixelsWide, pixelsTall];
+        this.pixelsWide = pixelsWide;
+        this.pixelsTall = pixelsTall;
+
+        for (int i = 0; i < pixelsTall; i++)
+            for (int j = 0; j < pixelsWide; j++)
+                rows[j, i] = int.Parse(numbers[i * pixelsWide + j].ToString());
+    }
+
+    public int GetNumber(int x, int y)
+    {
+        return rows[x, y];
+    }
+
+    public override string ToString()
+    {
+        string res = "";
+
+        for (int i = 0; i < pixelsTall; i++)
+        {
+            for (int j = 0; j < pixelsWide; j++)
+                res += rows[j, i];
+            if (i != pixelsTall - 1) res += "\n";
+        }
+
+        return res;
+    }
+
+    public int CountAparitions(int number)
+    {
+        int aparitions = 0;
+
+        for (int i = 0; i < pixelsTall; i++)
+            for (int j = 0; j < pixelsWide; j++)
+                if (rows[j, i] == number) aparitions++;
+
+        return aparitions;
+    }
+}
---
+++ AdventOfCode2019Tests/Day08SolverTests.cs
@@ -0,0 +1,34 @@
+﻿using AdventOfCode2019;
+using AdventOfCode2019Tests.Properties;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace AdventOfCode2019Tests
+{
+    [TestClass]
+    public class Day08SolverTests
+    {
+        [TestMethod]
+        [DataRow("123456789012", 3, 2, "123\n456\n\n789\n012")]
+        [DataRow("0222112222120000", 2, 2, "02\n22\n\n11\n22\n\n22\n12\n\n00\n00")]
+        public void ConstructLayersTest(string input, int pixelsWide, int pixelsTall, string expectedLayers)
+        {
+            Assert.AreEqual(expectedLayers, (new Day8Solver(input, pixelsWide, pixelsTall)).ConstructLayers());
+        }
+
+
+        [TestMethod]
+        public void Part1Test()
+        {
+            Assert.AreEqual(2080, (new Day8Solver(Resources.Day8Input, 25, 6)).SolvePart1());
+        }
+
+        [TestMethod]
+        public void Part2Test()
+        {
+            string finalImage = "0110010010111000110010001\n1001010010100101001010001\n1001010010100101000001010\n1111010010111001000000100\n1001010010101001001000100\n1001001100100100110000100";
+
+            Assert.AreEqual(finalImage.GetHashCode(), (new Day8Solver(Resources.Day8Input, 25, 6)).SolvePart2());
+        }
+    }
+}
